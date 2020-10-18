784080cf7e9c797e19b5997ec23c9890a1f8a47f
harmpreet <terapreethrmn@gmail.com>
2020-10-18 22:05:11 +0530

    my first website

---
+++ E-agriculture/agri/myagri/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from .models import Product
+from math import ceil
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request, 'index.html', params)
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def tracker(request):
+    return render(request, 'tracker.html')
+
+def search(request):
+    return render(request, 'search.html')
+
+def prodview(request, myid):
+    # Fetch the product using the id
+    product = Product.objects.filter(id=myid)
+
+
+    return render(request, 'shop/prodView.html', {'product':product[0]})
+
+def checkout(request):
+    return render(request, 'checkout.html')
